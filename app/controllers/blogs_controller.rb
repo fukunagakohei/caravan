ae60f01c5c8b38b9f2eb575cc4457dd3412e4527
fukunagakohei <syukatsukohei1@gmail.com>
2018-03-14 10:32:26 +0000

    week3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,33 @@
 class BlogsController < ApplicationController
 	def index
 	end
+
+	def new
+		@blog = Blog.new
+		# ブログモデルと紐付いた（データを入れるための）空の箱を定義
+		# インスタンスという。
+	end
+
+	def create
+		blog = Blog.new(blog_params)
+		# ビューから送られてきたデータをblogという空き箱に入れてる
+		blog.save
+		redirect_to "/blogs"
+	end
+
+	def index
+		@blogs = Blog.all
+	end
+
+	def show
+		@blog = Blog.find(params[:id])
+		# findメソッドはidを使ってモデルからレコードを探す
+	end
+
+	private
+	 def blog_params
+	   params.require(:blog).permit(:title, :body, :category)
+	   # ビューから送信されたデータはparamsというメソッドの中に入っている。
+	   # ストロングパラメータはセキュリティを強固にしてるもの。
+	 end
 end
--- test/controllers/blogs_controller_test.rb
+++ test/controllers/blogs_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class BlogsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new" do
+    get blogs_new_url
+    assert_response :success
+  end
+
 end
